83c0f7568e67519aa80c0a99a04272f770160d94
mjjang <inseok16@gmail.com>
2020-06-04 01:02:47 +0900

    튜토리얼 화면 추가

--- LolNotification/app/src/main/java/com/mjjang/lolnotification/TutorialActivity.kt
+++ LolNotification/app/src/main/java/com/mjjang/lolnotification/TutorialActivity.kt
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil.setContentView
 import com.mjjang.lolnotification.databinding.ActivityTutorialBinding
+import kotlinx.android.synthetic.main.activity_tutorial.*
 
 class TutorialActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView<ActivityTutorialBinding>(this, R.layout.activity_tutorial)
+
+        image_tutorial_close.setOnClickListener {
+            finish()
+        }
+
+        btn_tutorial_start.setOnClickListener {
+            finish()
+        }
     }
 }
