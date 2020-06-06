6b86ddfe7f73409ff3c81d6235e00f39398b6c9b
mjjang <inseok16@gmail.com>
2020-06-06 11:39:50 +0900

    뒤로가기 2번 눌러야 꺼지도록 수정

--- LolNotification/app/src/main/java/com/mjjang/lolnotification/MainActivity.kt
+++ LolNotification/app/src/main/java/com/mjjang/lolnotification/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mjjang.lolnotification
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil.setContentView
 import com.mjjang.lolnotification.databinding.ActivityMainBinding
 import com.mjjang.lolnotification.manager.AppPreference
@@ -18,5 +19,22 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, TutorialActivity::class.java)
             startActivity(intent)
         }
+
+
+    }
+
+    override fun onBackPressed() {
+        val systemTiem = System.currentTimeMillis()
+        if (systemTiem > backKeyPressTime + BACK_KEY_PRESS_TIME) {
+            backKeyPressTime = systemTiem
+            Toast.makeText(this, R.string.back_press_message, Toast.LENGTH_SHORT).show()
+        } else {
+            finish()
+        }
+    }
+
+    companion object {
+        var backKeyPressTime : Long = System.currentTimeMillis();
+        val BACK_KEY_PRESS_TIME : Int = 2000;
     }
 }
